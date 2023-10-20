32df20db6c69deffc2ce3f6f794a65906308e481
Sujith K S <kssujith92@gmail.com>
2023-10-20 10:04:52 +0800

    Update find_path_optimized.py

--- find_path_optimized.py
+++ find_path_optimized.py
@@ -1,52 +1,83 @@
 def solution(M):
-    def min_len(M,H,W):
+    def min_len(M,H,W,C):
         
-        Q=[(H-1,W-1,1)]
-        D=[] 
-        C=[]
+        Q=[(H-1,W-1,1000,1)]
+        XY=(H-1)*W+W-1
+        D={XY:[(1000,0)]}
    
         while Q:
             print(Q)
             q=Q.pop()
-            x,y,c=q[0],q[1],q[2]
+            x,y,pxy,c=q[0],q[1],q[2],q[3]
+            xy=x*W+y
             print('Current Step: ',q)
-            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            print('C=',C,'c=',c)
+            if c>=C:
+                continue
+            for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
                 nx,ny=x+dx,y+dy
                 print(nx,ny,c)
-                if 0<=nx<H and 0<=ny<W:
+                nxy=nx*W+ny
+                if 0<=nx<H and 0<=ny<W and pxy!=nxy:
                     if (nx,ny)==(0,0):
-                        C.append(c+1)
+                        if c+1<C: 
+                            C=c+1
                         print('Target reached. c=',c+1)
                     elif M[nx][ny]==0:
-                        if nx*W+ny in D:
-                            print('Visited')
-                            continue
-                        Q.append((nx,ny,c+1))
-            D.append(x*W+y)
-            #print(D)
-        print(C)
-        return min(C)
+                        if nxy in D:
+                            if (xy,1) in D[nxy]:
+                                    continue
+                        else:
+                            D[nxy]=[]
+                        Q.append((nx,ny,xy,c+1))
+                        D[nxy].append((xy,0))
+            Q.sort()
+            D[xy].append((pxy,1))
+        return C
     
     H=len(M)
     W=len(M[0])
-    #l=500
-    return min_len(M, H, W)
+    l= min_len(M,H,W,H*W)
     
     
-#    for i in range(H):
- #       for j in range(W):
-  #          if Map[i][j]==1:
-   #             print(i,j)
-    #            Mt=list(Map)
-     #           Mt[i][j]=0
-      #          lt=min_len(Mt,H,W)
-       #         if l>lt: l=lt
-   # return l
+    for i in range(H):
+        for j in range(W):
+            if M[i][j]==1:
+                print(i,j)
+                Mt=list(M)
+                Mt[i][j]=0
+                lt=min_len(Mt,H,W,l)
+                if l>lt: l=lt
+    return l
 
-print(solution([[0, 0, 0, 0, 0, 0], 
-                [1, 1, 0, 1, 1, 0], 
-                [0, 0, 0, 0, 0, 0], 
-                [0, 1, 1, 1, 1, 1], 
-                [0, 1, 1, 1, 1, 1], 
-                [0, 0, 0, 0, 0, 0]]))
-           
+print(solution([[0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0], 
+                [0, 1, 1, 1, 1, 1, 1], 
+                [0, 0, 0, 0, 0, 0, 0], 
+                [1, 1, 1, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 1, 1, 1, 1], 
+                [0, 0, 0, 0, 0, 0, 0]]))
+
+#print(solution([[0, 0, 0, 0, 0, 0], 
+ #               [1, 1, 1, 1, 1, 0], 
+  #              [0, 0, 0, 0, 0, 0], 
+   #             [0, 1, 1, 1, 1, 1], 
+    #            [0, 1, 1, 1, 1, 1], 
+     #           [0, 0, 0, 0, 0, 0]])) 
+   
+#print(solution([[0,0,0,0],
+ #               [1,1,1,0],
+  #              [1,0,0,0],
+   #             [1,0,0,0]]))
+
+#import numpy.random as r
+#import time
+#t=time.time()
+#M=r.randint(0,1,size=(10,10))
+#print(M)
+#print(solution(M))
+#print('Elapsed time =',time.time()-t)
+
+    
